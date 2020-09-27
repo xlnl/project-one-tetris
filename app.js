f81d7218ec64ada647f6f7bb8546cc701e8b14c4
xlnl <lamanhnle@gmail.com>
2020-09-27 00:03:01 -0400

    more html and css set up for basic game design, pseudocoding process and working through defining variables

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-console.log('hewwo')
+// PSUEDOCODING
+
+// HTML set up -> create 288 divs with a container div with class 
+//     Create a start/pause button
+//     Create a score + player section (update later to allow for name input form button??)
+// CSS set up -> Styling the divs (basic for now) -> each div (20x20px) -> board (12x24divs)
+//     game board = width 240px; height: 480px
+// JS set up 
+//     variables + constants
+//         game pieces/tetrominoes
+//         drawing them and their rotations (create separate excel sheet with blueprint of all game pieces & their rotations)
+//         store the rotations of the game pieces in an array under their respective constants
+//              const lTetro 
+//              const zTetro 
+//              const oTetro 
+//              const iTetro 
+//              const tTetro 
+//              const pieces = [lTetro, zTetro, oTetro, iTetro, tTetro]
+//         game board
+//              const board = document.querySelector('.board')
+//              use Array.from to grab all the divs/units and put them into an array (re: tictactoe)
+//              let units = Array.from(document.querySelectorAll('.board div'))
+//         score board
+//              const scoreBoard = document.querySelector('#score')
+//         start/pause button
+//              const startBtn = document.querySelector('#start-button')
+//     functions
+//         forEach function
+//              
+
+
+document.addEventListener('DOMContentLoaded' () => {
+    console.log('hewwo')
+})
